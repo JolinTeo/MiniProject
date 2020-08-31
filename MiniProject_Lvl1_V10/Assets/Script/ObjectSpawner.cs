3808d9ae82ef5f57fa2834dfef45b03bd7ec3ba3
Henrypoh258 <Henrypoh258@gmail.com>
2020-08-31 16:42:44 +0800

    Commit by Jolin
    Updated level 1

---
+++ MiniProject_Lvl1_V10/Assets/Script/ObjectSpawner.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectSpawner : MonoBehaviour
+{
+    [SerializeField] public GameObject movingObj;
+    [SerializeField] private Transform spawnPos;
+    [SerializeField] private float miniSeparationTime;
+    [SerializeField] private float maxSeparationTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(spawnVehicle());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private IEnumerator spawnVehicle()
+    {
+        while(true)
+        {
+            yield return new WaitForSeconds(Random.Range(miniSeparationTime, maxSeparationTime));
+            GameObject GO = Instantiate(movingObj, spawnPos.position, Quaternion.identity);
+            GO.transform.eulerAngles = new Vector3(0, 90, 0);
+        }
+        
+    }
+}
---
+++ MiniProject_Lvl1_V10/Assets/Script/Player.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] public TerrainGenerator terrainGenerator;
+    [SerializeField] private Text scoreText;
+    [SerializeField]
+    private int Score;
+    private Animator animator;
+    private bool isHopping;
+
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        if(Score >= 300)
+        {
+            SceneManager.LoadScene("Level2");
+        }
+
+        scoreText.text = "Score : " + Score;
+        if (Input.GetKeyDown(KeyCode.W) && !isHopping)
+        {
+            Score += 10;
+           
+            float xDifference = 0;
+            if(transform.position.x % 1 == 0)
+            {
+                xDifference = Mathf.Round(transform.position.x) - transform.position.x;                
+            }
+            MoveCharacter(new Vector3(xDifference, 0, 1));
+            transform.rotation = Quaternion.Euler(0, 180, 0);
+        }
+        
+        else if(Input.GetKeyDown(KeyCode.A) && !isHopping)
+        {
+            MoveCharacter(new Vector3(-1, 0, 0));
+            transform.rotation = Quaternion.Euler(0, 90, 0);
+        }
+
+        else if (Input.GetKeyDown(KeyCode.D) && !isHopping)
+        {
+            MoveCharacter(new Vector3(1, 0, 0));
+            transform.rotation = Quaternion.Euler(0, -90, 0);
+        }
+
+        else if (Input.GetKeyDown(KeyCode.S) && !isHopping)
+        {
+            MoveCharacter(new Vector3(0, 0, -1));
+            transform.rotation = Quaternion.Euler(0, 360, 0);
+            Score -= 10;
+        }
+    }
+
+    private void MoveCharacter(Vector3 difference)
+    {
+        animator.SetTrigger("Hop");
+        isHopping = true;
+        transform.position = (transform.position + difference);
+        terrainGenerator.SpawnTerrain(false, transform.position);
+    }
+
+    public void FinishHop()
+    {
+        isHopping = false;
+    }
+
+}
