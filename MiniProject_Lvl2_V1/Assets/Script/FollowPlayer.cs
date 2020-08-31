24bf95c544f92670c8be1db9f720ab8ac1752542
Henrypoh258 <Henrypoh258@gmail.com>
2020-08-31 16:37:39 +0800

    Commit by Henry
    Updated by Henry and Jolin

---
+++ MiniProject_Lvl2_V1/Assets/Script/FollowPlayer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    [SerializeField] private GameObject player;
+    [SerializeField] private Vector3 offset;
+    [SerializeField] private float smoothness;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(player != null)
+        {
+            transform.position = Vector3.Lerp(transform.position, player.transform.position + offset, smoothness);
+
+        }
+        
+    }
+}
