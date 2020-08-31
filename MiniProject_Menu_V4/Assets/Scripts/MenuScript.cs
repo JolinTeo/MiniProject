94d3505581ad3e4fe87d4a61d5730f792f48783f
Henrypoh258 <Henrypoh258@gmail.com>
2020-08-31 16:14:41 +0800

    Commit by Henry
    Menu V4 - Pause Panel.

---
+++ MiniProject_Menu_V4/Assets/Scripts/MenuScript.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Menu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void Play()
+    {
+        SceneManager.LoadScene("Level 1");
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public void Mute()
+    {
+        AudioListener.pause = !AudioListener.pause;
+    }
+
+    public void Pause()
+    {
+        Time.timeScale = 0;
+    }
+
+    public void Unpause()
+    {
+        Time.timeScale = 1;
+    }
+
+    public void RestartLevel1()
+    {
+        SceneManager.LoadScene("Level 1");
+    }
+}
